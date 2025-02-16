16139bf4f4a3d495fdad960d21e0e1fb6ca6c651
Evelyn Stangneth <evelyn@kivdul.biologie.hu-berlin.de>
2025-02-16 19:47:22 +0100

    started editing big model

--- Patrick/run.py
+++ Patrick/run.py
@@ -7,8 +7,8 @@ species_names = ["EGF", "EGFR", "Xa", "XA", "EGF_EGFR", "EGF_EGFR2",
 sm = SimManager(parameters=p,
                 species_names=species_names,
                 endt = 1,
-                mesh_path = "/home/evelyn/shared_files/signaling_repo/steps_cell_signaling/Patrick/meshes/.inp/kugel_7.5.inp",
-                save_file ="saved_objects/initial_run/parallel_run.h5",
+                mesh_path = "/home/evelyn/shared_files/signaling_repo/steps_cell_signaling/Patrick/meshes/.inp/kugel04.inp",
+                save_file ="saved_objects/initial_run/parallel_run", # no .h5 ending needs to be added
                 parallel = True,
                 runname = "parallel_run")
 sm.load_model(type="small")
